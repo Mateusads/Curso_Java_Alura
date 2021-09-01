6482525fee63557dd9807dd6a84b5d9735f265c7
Mateus Medeiros <mateus.medeiros@cingo.local>
2021-09-01 16:53:27 -0300

    metodo saca e deposita

--- bytebank/src/bytebank/Conta.java
+++ bytebank/src/bytebank/Conta.java
@@ -6,5 +6,18 @@ public class Conta {
 	int agencia;
 	int numero;
 	String titular;	
+	
+	
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
+	
+	boolean saca(double valor) {
+		if(saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		}else
+			return false;
+	}
 
 }
--- bytebank/src/bytebank/CriarConta.java
+++ bytebank/src/bytebank/CriarConta.java
@@ -4,17 +4,30 @@ public class CriarConta {
 
 	public static void main(String[] args) {
 		Conta primeiraConta = new Conta();
-		primeiraConta.saldo = 200;
-		System.out.println(primeiraConta.saldo);
-
-		primeiraConta.saldo += 100;
-		System.out.println(primeiraConta.saldo);
-
-		Conta segundaConta = primeiraConta;
-		segundaConta.saldo = 50;
-
-		System.out.println("primeira conta tem " + primeiraConta.saldo);
-		System.out.println("segunda conta tem " + segundaConta.saldo);
+		/*
+		 * primeiraConta.saldo = 200; System.out.println(primeiraConta.saldo);
+		 * primeiraConta.saldo += 100; System.out.println(primeiraConta.saldo); Conta
+		 * segundaConta = primeiraConta; segundaConta.saldo = 50;
+		 * System.out.println("primeira conta tem " + primeiraConta.saldo);
+		 * System.out.println("segunda conta tem " + segundaConta.saldo);
+		 */
+		
+		System.out.println("Deposita 100");
+		primeiraConta.deposita(100);
+		System.out.println("Valor do saldo atual = " + primeiraConta.saldo);
+		
+		System.out.println("Saca 75");
+		boolean retorno = primeiraConta.saca(75);
+		System.out.println("Tem o valor  = " + retorno);
+		System.out.println("Valor do saldo atual = " + primeiraConta.saldo);
+		
+		System.out.println("Saca 80");
+		retorno = primeiraConta.saca(80);
+		System.out.println("Tem o valor  = " + retorno);
+		System.out.println("Valor do saldo atual = " + primeiraConta.saldo);
+		
+		
+		
 	}
 
 }
