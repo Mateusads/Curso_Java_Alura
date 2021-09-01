5b6a124fb18dee1e66ff3c6d047e57ac06cf4fbd
Mateus Medeiros <mateus.medeiros@cingo.local>
2021-09-01 16:26:09 -0300

    cria conta

--- bytebank/src/bytebank/CriarConta.java
+++ bytebank/src/bytebank/CriarConta.java
@@ -3,9 +3,18 @@ package bytebank;
 public class CriarConta {
 
 	public static void main(String[] args) {
-		
 		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		System.out.println(primeiraConta.saldo);
 
+		primeiraConta.saldo += 100;
+		System.out.println(primeiraConta.saldo);
+
+		Conta segundaConta = primeiraConta;
+		segundaConta.saldo = 50;
+
+		System.out.println("primeira conta tem " + primeiraConta.saldo);
+		System.out.println("segunda conta tem " + segundaConta.saldo);
 	}
 
 }
