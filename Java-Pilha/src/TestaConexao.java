6af4cb3b0857a3e9a91939b9b1bb055083bc2949
Mateus Medeiros <mateus.medeiros@cingo.local>
2021-09-03 11:56:45 -0300

    usando try catch com finally

---
+++ Java-Pilha/src/TestaConexao.java
@@ -0,0 +1,17 @@
+
+public class TestaConexao {
+	
+	public static void main(String[] args) {
+		
+	    Conexao con = null;
+	    try {
+	        con = new Conexao();
+	        con.leDados();
+	    } catch(IllegalStateException ex) {
+	        System.out.println("Deu erro na conexao");	        
+	    } finally {
+	    	con.fecha();
+		}
+	}
+
+}
